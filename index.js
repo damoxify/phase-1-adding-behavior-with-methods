213d49bdafa231fec4add9d6745ebcfdc905be06
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-28 19:44:35 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+class Cat {
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+
+class Dog {
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name){
+        this.name = name
+ 
+
+    }
+    speak(){
+        return `It's me! ${this.name}, the parrot!`
+    }
+
+}
+
